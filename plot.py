b5577b5e7e97a8615677000e8147fca3903c179f
AGorchakov <andr113@mail.ru>
2019-02-06 12:33:13 +0300

    Увеличено количество рисуемых слоев (*3)

--- plot.py
+++ plot.py
@@ -2,13 +2,13 @@ import matplotlib.pyplot as plt
 
 def plt2Dlat(x, L, R, deltaH=0):
     H = 0.0
-    for i in range(3, len(x), 3):
+    for i in range(3, 3*len(x), 3):
         H = H + x[(i+3*deltaH)%len(x)]
     fig, ax = plt.subplots(figsize=(L+4*R, H+4*R))
     plt.xlim(-2*R, L+2*R)
     plt.ylim(-2*R, H+2*R)
     h = 0.0
-    for i in range(0, len(x), 3):
+    for i in range(0, 3*len(x), 3):
         l = x[(i+3*deltaH)%len(x)+1]
         while l <= L:
             circle1 = plt.Circle((l, h), R, color='blue')
@@ -22,4 +22,4 @@ x = 0.730256,0,2.52968,1.46051,0,2.52968,0.730256,1.26484,2.52968,1.46051,1.2648
     
 L = 16
 R=0.3
-plt2Dlat(x, L, R, 3)
+plt2Dlat(x, L, R, 0)
